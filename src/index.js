5e4e60c05696d1d94ae601a1d7345a4b2ddaa5d9
Quoc Vu <quoc_vu@intuit.com>
2016-01-15 14:00:05 -0800

    first draft

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var config = require('config'),
+  command = require('commander');
+
+console.log('Hello World :)');
---
+++ test/index.js
@@ -0,0 +1,8 @@
+var should = require('should'),
+  sinon = require('sinon');
+
+describe('My test suite', function () {
+  it('should do nothing', function () {
+
+  })
+});
