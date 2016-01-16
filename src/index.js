6ab19c2678cbe0c152df2a48cba67f1a3a562c64
Quoc Vu <quoc_vu@intuit.com>
2016-01-15 18:38:07 -0800

    testing hound

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ var config = require('config'),
   command = require('commander');
 
 console.log('Hello World :)');
+
+var p = new Promise(function (resolve, reject) {
+  console.log('this line has no ;')
+    console.log('bad indent');
+});
