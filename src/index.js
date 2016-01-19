7e9f02b410a26eabcf30362050c7e9bea04bf736
Quoc Vu <quoc_vu@intuit.com>
2016-01-19 15:19:36 -0800

    adding coveralls

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@
 var config = require('config'),
   command = require('commander');
 
-console.log('Hello World :)');
+module.exports = {
+  older: function (a, b) {
+    if (a.age > b.age) {
+      return a.name + ' is older than ' + b.name;
+    } else if (b.age > a.age) {
+      return b.name + ' is older than ' + a.name;
+    } else {
+      return a.name + ' and ' + b.name + ' have the same age';
+    }
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
 var should = require('should'),
-  sinon = require('sinon');
+  sinon = require('sinon')
+  my = require('../src/index.js');
 
 describe('My test suite', function () {
-  it('should do nothing', function () {
+  var ben = { name: 'Ben', age: 31 },
+    frank = { name: 'Frank', age: 27 },
+    john = { name: 'John', age: 27 };
 
+  it('should assess who is older', function () {
+    my.older(ben, frank).should.be.equal('Ben is older than Frank');
+    my.older(frank, ben).should.be.equal('Ben is older than Frank');
+    my.older(john, frank).should.be.equal('John and Frank have the same age');
   })
 });
